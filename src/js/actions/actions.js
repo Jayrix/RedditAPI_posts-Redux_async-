3b91f894f9e13fbbbbd39544aa13ba055304f934
Jakub Kozlik <kozlik.jakub@gmail.com>
2017-12-17 23:51:50 +0100

    actions

--- src/js/actions/actions.js
+++ src/js/actions/actions.js
@@ -24,3 +24,14 @@ function requestPosts(subreddit) {
         subreddit
     }
 }
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+
+function receivePosts(subreddit, json) {
+    return {
+        type: RECEIVE_POSTS,
+        subreddit,
+        posts: json.data.children.map(child => child.data),
+        receivedAt: Date.now()
+    }
+}
